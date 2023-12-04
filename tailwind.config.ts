f1925bece93a2d0a0bdfa87e564bc62a0b5c8d95
vignesh <vigneshanand17@gmail.com>
2023-12-04 15:50:28 +0530

    Added homepage - mobile view.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,49 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        edgeset: "#215f74"
+      },
+      height: {
+      },
+      maxHeight: {
+        '100': '30rem',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      gridTemplateColumns: {
+        '18': 'repeat(18, minmax(0, 1fr))',
+      },
+      gridColumn: {
+        'span-16': 'span 16 / span 16'
+      },
+      keyframes: {
+        'fade-in-down': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(10px)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          },
+        },
+        'conveyor': {
+          '0%': {
+            transform: 'translateY(0)'
+          },
+          '100%': {
+            transform: 'translateY(-100%)'
+          }
+        }
+      },
+      animation: {
+        'fade-in-down': 'fade-in-down 0.5s ease-out',
+        'conveyor': 'conveyor 60s linear infinite'
+      }
     },
   },
   plugins: [],
