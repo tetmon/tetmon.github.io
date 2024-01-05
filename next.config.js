66c2ec28f424ac50d5c1723dd6658c3ba5ddd917
vignesh <vigneshanand17@gmail.com>
2024-01-05 20:22:48 +0530

    Removed assetsPrefix.

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const isProd = process.env.NODE_ENV === 'production'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: isProd ? 'export' : undefined,
-  assetPrefix: isProd ? 'https://www.tetmon.com/staging.tetmon.github.io/' : undefined,
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   images: {
     unoptimized: true
